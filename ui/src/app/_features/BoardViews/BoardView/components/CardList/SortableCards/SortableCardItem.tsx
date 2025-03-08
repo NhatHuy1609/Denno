551be1bbdc63141162625feeea7994ad4e687f21
Nhathuy <phamvannhathuy160903@gmail.com>
2025-03-08 18:27:26 +0700

    fix(ui): Implement sortable context for sortable cardlists and sortable cards

--- ui/src/app/_features/BoardViews/BoardView/components/CardList/SortableCards/SortableCardItem.tsx
+++ ui/src/app/_features/BoardViews/BoardView/components/CardList/SortableCards/SortableCardItem.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import { cardTypes } from '@/entities/card'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
-function SortableCardItem() {
-  return <div>SortableCardItem</div>
+interface Props {
+  cardData: cardTypes.Card
+}
+
+function SortableCardItem({ cardData }: Props) {
+  const { id, name } = cardData
+
+  const {
+    setNodeRef,
+    setActivatorNodeRef,
+    listeners,
+    isDragging,
+    isSorting,
+    over,
+    overIndex,
+    transform,
+    transition
+  } = useSortable({
+    id
+  })
+
+  const style: React.CSSProperties = {
+    transform: transform ? CSS.Translate.toString(transform) : undefined,
+    transition: transition ?? undefined,
+    padding: '4px',
+    border: '1px solid blue'
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...listeners}
+      style={style}
+      className='h-[60px] w-full border border-blue-500'
+    >
+      {name}
+    </div>
+  )
 }
 
 export default SortableCardItem
