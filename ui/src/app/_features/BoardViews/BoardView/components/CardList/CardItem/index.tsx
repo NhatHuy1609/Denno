5065c22ed610649fb4b2bda7c904d8509b7188c8
Nhathuy <phamvannhathuy160903@gmail.com>
2025-03-08 23:36:39 +0700

    feat(ui): Adding drag overlay for sortable card item

--- ui/src/app/_features/BoardViews/BoardView/components/CardList/CardItem/index.tsx
+++ ui/src/app/_features/BoardViews/BoardView/components/CardList/CardItem/index.tsx
@@ -1,14 +1,39 @@
-import { cardTypes } from '@/entities/card'
 import React from 'react'
+import { cardTypes } from '@/entities/card'
+import { Transform, CSS } from '@dnd-kit/utilities'
+import { DraggableSyntheticListeners } from '@dnd-kit/core'
 
 interface Props {
   cardData?: cardTypes.Card
+  dragging?: boolean
+  transform?: Transform | null
+  transition?: string | null
+  listeners?: DraggableSyntheticListeners
 }
 
-function CardItem({ cardData }: Props) {
-  const { name } = cardData || {}
+const CardItem = React.forwardRef<HTMLDivElement, Props>(
+  ({ cardData, dragging, transform, transition, listeners, ...props }, ref) => {
+    const { name } = cardData || {}
 
-  return <div>{name}</div>
-}
+    const style: React.CSSProperties = {
+      transform: transform ? CSS.Translate.toString(transform) : undefined,
+      transition: transition ?? undefined,
+      padding: '4px',
+      border: '1px solid blue'
+    }
+
+    return (
+      <div
+        ref={ref}
+        style={style}
+        className='h-[60px] w-full border border-blue-500'
+        {...props}
+        {...listeners}
+      >
+        {name}
+      </div>
+    )
+  }
+)
 
 export default CardItem
--- ui/src/app/_features/BoardViews/BoardView/components/CardList/SortableCards/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function SortableCards() {
-  return <div>index</div>
-}
-
-export default SortableCards
