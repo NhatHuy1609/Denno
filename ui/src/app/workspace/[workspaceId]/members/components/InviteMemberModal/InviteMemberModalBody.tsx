71f6b7135cbe74b18d9003bc795090dd1e1b9159
Nhathuy <phamvannhathuy160903@gmail.com>
2025-03-19 23:50:43 +0700

    fix(server): Fixed paginated users  responses

--- ui/src/app/workspace/[workspaceId]/members/components/InviteMemberModal/InviteMemberModalBody.tsx
+++ ui/src/app/workspace/[workspaceId]/members/components/InviteMemberModal/InviteMemberModalBody.tsx
@@ -2,29 +2,32 @@ import React, { useState, useRef } from 'react'
 import PrimaryInputText from '@/app/_components/PrimaryInputText'
 import { useUsersQuery } from '@/app/_hooks/query/user/useUsersQuery'
 import { userTypes } from '@/entities/user'
+import { useDebounce } from '@/app/_hooks/useDebounce'
 
 type SearchedUserFilter = Pick<userTypes.UsersFilterQuery, 'email'>
 
 function InviteMemberModalBody() {
   const inputRef = useRef<HTMLInputElement>(null)
-  const [searchedUserFilter, setSearchedUserFilter] = useState<SearchedUserFilter>({
-    email: ''
-  })
+
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const debouncedSearchTerm = useDebounce(searchTerm, 500)
+  const searchedUserFilter: SearchedUserFilter = {
+    email: debouncedSearchTerm
+  }
+
   const { data } = useUsersQuery(searchedUserFilter)
 
-  console.log('SEARCHED USER')
+  console.log('SEARCHED USER: ', data)
 
-  const handleSearchUser = () => {
-    setSearchedUserFilter({
-      email: inputRef.current?.value
-    })
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
   }
 
   return (
     <div className='flex'>
       <PrimaryInputText
         ref={inputRef}
-        // onInput={handleSearchUser}
+        onInput={handleInputChange}
         className='w-full border-gray-500 p-2'
         placeholder='Email address or name'
       />
