17f31c068e263aac138facb79831bb679fc07eea
Nhathuy <phamvannhathuy160903@gmail.com>
2025-06-20 23:12:00 +0700

    feat(server): Added Results pattern, add more action strategy when sent board join request

--- server/server/Controllers/WorkspacesController.cs
+++ server/server/Controllers/WorkspacesController.cs
@@ -443,6 +443,64 @@ namespace server.Controllers
 
             return Ok();
         }
+
+        [HttpPost("[controller]/joinRequests/{requestId}/approval")]
+        public async Task<IActionResult> AppoveWorkspaceJoinRequestAsync(int requestId)
+        {
+            var joinRequest = await _unitOfWork.JoinRequests.GetJoinRequestByIdAsync(requestId);
+
+            if (joinRequest == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var actionContext = new DennoActionContext()
+            {
+                MemberCreatorId = userId,
+                WorkspaceId = joinRequest.WorkspaceId,
+                TargetUserId = joinRequest.RequesterId
+            };
+
+            var action = await _actionService.CreateActionAsync(ActionTypes.ApproveWorkspaceJoinRequest, actionContext);
+
+            if (action != null)
+            {
+                await _emailService.SendActionEmailAsync(action);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("[controller]/joinRequests/{requestId}/rejection")]
+        public async Task<IActionResult> RejectWorkpsaceJoinRequestAsync(int requestId)
+        {
+            var joinRequest = await _unitOfWork.JoinRequests.GetJoinRequestByIdAsync(requestId);
+
+            if (joinRequest == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var actionContext = new DennoActionContext()
+            {
+                MemberCreatorId = userId,
+                WorkspaceId = joinRequest.WorkspaceId,
+                TargetUserId = joinRequest.RequesterId
+            };
+
+            var action = await _actionService.CreateActionAsync(ActionTypes.RejectWorkspaceJoinRequest, actionContext);
+
+            if (action != null)
+            {
+                await _emailService.SendActionEmailAsync(action);
+            }
+
+            return Ok();
+        }
     }
 }
 
---
+++ server/server/Dtos/Requests/Board/CreateBoardJoinRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Dtos.Requests.Board
+{
+    public class CreateBoardJoinRequest
+    {
+        [Required]
+        public string RequesterId { get; set; }
+    }
+}
