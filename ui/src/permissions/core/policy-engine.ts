f1b55440d2c60700f3f739ac1830b24ad65982eb
Nhathuy <phamvannhathuy160903@gmail.com>
2025-07-16 22:23:25 +0700

    feat(ui): Add PermissionGate component and usePermissions hook for enhanced access control

--- ui/src/permissions/core/policy-engine.ts
+++ ui/src/permissions/core/policy-engine.ts
@@ -4,7 +4,6 @@ import {
   PolicyContext, 
   PolicyResult,
   PolicyKey,
-  IPolicy 
 } from './types'
 import { PolicyRegistry } from './policy-registry'
 
---
+++ ui/src/permissions/hooks/usePermissions.ts
@@ -0,0 +1,43 @@
+import { useMe } from "@/app/_hooks/query/user/useMe"
+import { PolicyEngine } from "../core/policy-engine"
+import { PolicyAction, PolicyContext, PolicyResource } from "../core/types"
+
+export const usePermissions = () => {
+  const {data: currentUser } = useMe()
+
+  const policyEngine = new PolicyEngine()
+
+  const can = <T = any>(
+    action: PolicyAction,
+    resource: PolicyResource,
+    resourceData?: T
+  ): boolean => {
+    const context: PolicyContext = {
+      user: currentUser!,
+    }
+
+    return policyEngine.can(action, resource, context, resourceData)
+  }
+  
+  const cannot = <T = any>(
+    action: PolicyAction, 
+    resource: PolicyResource, 
+    resourceData?: T
+  ): boolean => {
+    return !can(action, resource, resourceData);
+  };
+  
+  const canWithReason = <T = any>(
+    action: PolicyAction, 
+    resource: PolicyResource, 
+    resourceData?: T
+  ) => {
+    const context: PolicyContext = {
+      user: currentUser!,
+    };
+    
+    return policyEngine.canWithReason(action, resource, context, resourceData);
+  };
+
+  return { can, cannot, canWithReason }
+}
