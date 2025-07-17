a5890c7f3cc27b70b2b4fd4a9f465762fb25d9cb
Nhathuy <phamvannhathuy160903@gmail.com>
2025-07-17 12:01:39 +0700

    feat(ui): Implement board access control policies and sync recent access with local storage

--- ui/src/app/board/[boardId]/page.tsx
+++ ui/src/app/board/[boardId]/page.tsx
@@ -1,49 +1,21 @@
 'use client'
 
-import React, { useEffect } from 'react'
-import { PersistedStateKey } from '@/data/persisted-keys'
-import { useParams, useRouter } from 'next/navigation'
-import { useSyncedLocalStorage } from '@/app/_hooks/useSyncedLocalStorage'
-import { useBoardQuery } from '@/app/_hooks/query'
+import React from 'react'
+import { useParams } from 'next/navigation'
 import { useMe } from '@/app/_hooks/query/user/useMe'
-import { includesBy } from '@/utils/includesBy'
 import BoardView from '@/app/_features/BoardViews/BoardView'
 import PrivateBoardAccessRequest from './PrivateBoardAccessRequest'
+import useBoardPolicyAccess from '@/permissions/hooks/useBoardPolicyAccess'
+import useRecentAccessSync from './useRecentAccessSync'
 
 function BoardHomePage() {
-  const router = useRouter()
-  const { data: currentUser } = useMe()
   const { boardId } = useParams<{ boardId: string }>()
-  const { data: boardData } = useBoardQuery(boardId, {
-    includeWorkspace: true,
-    includeBoardMembers: true
-  })
-  const [_1, setBoardId] = useSyncedLocalStorage(PersistedStateKey.RecentAccessBoard, '')
-  const [_2, setWorkspaceId] = useSyncedLocalStorage(PersistedStateKey.RecentAccessWorkspace, '')
+  const { workspaceId = '', canView: canViewBoard } = useBoardPolicyAccess(boardId)
 
-  const { workspaceId, members } = boardData || {}
-
-  // Sync recent access board id with local storage
-  useEffect(() => {
-    boardId && setBoardId(boardId as string)
-  }, [boardId])
-
-  // Sync recent access workspace id with local storage
-  useEffect(() => {
-    workspaceId && setWorkspaceId(workspaceId as string)
-  }, [workspaceId])
+  useRecentAccessSync(boardId, workspaceId)
 
   // Checks if the user should be denied access to the board.
-  const shouldBlockAccess = (): boolean => {
-    if (!currentUser || !members) return false
-
-    var isWorkspaceMember = includesBy(members, (member) => member.memberId === currentUser.id)
-    var isBoardPrivate = boardData?.visibility === 'Private'
-
-    return !isWorkspaceMember || isBoardPrivate
-  }
-
-  if (shouldBlockAccess()) {
+  if (!canViewBoard) {
     return <PrivateBoardAccessRequest />
   }
 
