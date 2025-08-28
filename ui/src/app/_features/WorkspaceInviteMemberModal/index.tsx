0e9be2196800815fa5ee1395090bfbd297ba9803
Nhathuy <phamvannhathuy160903@gmail.com>
2025-08-28 10:08:25 +0700

    feat(ui): implement DialogWrapper component and refactor WorkspaceInviteMemberModal to use it

--- ui/src/app/_features/WorkspaceInviteMemberModal/index.tsx
+++ ui/src/app/_features/WorkspaceInviteMemberModal/index.tsx
@@ -1,37 +1,22 @@
-import React, { useState } from 'react'
-import { AiOutlineUserAdd } from 'react-icons/ai'
-import { Dialog } from '@/ui'
-import CustomizableButton from '@/ui/components/CustomizableButton'
-
+import React from 'react'
 import InviteMemberModalBody from './InviteMemberModalBody'
+import DialogWrapper from '@/app/_components/DialogWrapper'
 
-function WorkspaceInviteMemberModal() {
-  const [showInviteMemberModal, setShowInviteMemberModal] = useState(false)
+type Props = {
+  renderTrigger: () => React.ReactNode
+}
 
-  const handleCloseModal = () => {
-    setShowInviteMemberModal(false)
-  }
-
+function WorkspaceInviteMemberModal({ renderTrigger }: Props) {
   return (
-    <Dialog.Dialog onOpenChange={setShowInviteMemberModal} open={showInviteMemberModal}>
-      <Dialog.DialogTrigger asChild>
-        <CustomizableButton
-          value='Invite Workspace members'
-          size='medium'
-          intent='primary'
-          startIcon={<AiOutlineUserAdd />}
-        />
-      </Dialog.DialogTrigger>
-      <Dialog.DialogContent aria-describedby={undefined} className='min-w-[640px]'>
-        <Dialog.DialogHeader>
-          <Dialog.DialogTitle>
-            <span className='text-xl font-normal'>Invite to Workspace</span>
-          </Dialog.DialogTitle>
-        </Dialog.DialogHeader>
-        {/* Body */}
-        <InviteMemberModalBody closeModalFn={handleCloseModal} />
-      </Dialog.DialogContent>
-    </Dialog.Dialog>
+    <DialogWrapper
+      renderTrigger={renderTrigger}
+      renderContent={(closeModalFn) => (
+        <div className='w-auto bg-white'>
+          <InviteMemberModalBody closeModalFn={closeModalFn} />
+        </div>
+      )}
+      title='Invite to Workspace'
+    />
   )
 }
 
