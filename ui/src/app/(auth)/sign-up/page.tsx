a30a710dbe25416a9b9704144886c644af12e316
Nhathuy <phamvannhathuy160903@gmail.com>
2025-09-20 03:52:33 +0700

    fix import path

--- ui/src/app/(auth)/sign-up/page.tsx
+++ ui/src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SignUpForm } from '@/app/_features/Signup/SignUpForm'
+import SignUpForm from '@/app/_features/Signup/SignUpForm/SignUpForm'
 
 export default function Page() {
   return <SignUpForm />
--- ui/src/app/_features/signup/SignUpVerificationForm/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as SignUpVerificationForm} from './SignUpVerificationForm'
-export * from './SignUpVerificationOTP'
