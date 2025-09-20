f3ff3f42151727ee402f1dbb55671d45a36ed36c
Nhathuy <phamvannhathuy160903@gmail.com>
2025-09-20 22:47:50 +0700

    refactor(CardItem): replace button with Link for navigation
    - Changed the CardItem component to use a Link instead of a button for navigation.
    - Updated the href to point directly to the card's URL.
    - Removed the onClick handler as it is no longer necessary with Link.

--- ui/src/app/_features/BoardViews/BoardView/components/CardItem/index.tsx
+++ ui/src/app/_features/BoardViews/BoardView/components/CardItem/index.tsx
@@ -42,10 +42,10 @@ function CardItem({ cardData, isDragging = false }: Props) {
   }
 
   return (
-    <button
-      onClick={handleClick}
+    <Link
+      // onClick={handleClick}
       // href={`/board/${boardId}/card/${cardData.id}`}
-      // href={`/card/${cardData.id}`}
+      href={`/card/${cardData.id}`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className='flex w-full flex-col gap-2 rounded-lg border-2 border-transparent bg-white px-3 py-[6px] shadow-[0_1px_1px_rgba(0,0,0,0.15)] hover:border-2 hover:border-blue-500'
@@ -53,7 +53,7 @@ function CardItem({ cardData, isDragging = false }: Props) {
       <CardTitleRow isHover={isHover} cardData={cardData} />
       <CardDetailsInfoRow cardData={cardData} />
       <CardMemberInfoRow cardId={cardData.id} />
-    </button>
+    </Link>
   )
 }
 
