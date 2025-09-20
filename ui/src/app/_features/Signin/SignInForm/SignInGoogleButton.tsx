a9c0a462a51c674d3af7a347bd25e7b02b742d20
Nhathuy <phamvannhathuy160903@gmail.com>
2025-09-20 12:43:44 +0700

    fix: fix path

---
+++ ui/src/app/_features/Signin/SignInForm/SignInGoogleButton.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import GoogleIcon from 'public/google.png'
+import { Button, messageError } from '@/ui'
+import Image from 'next/image'
+import { AuthService } from '@/service/api/auth'
+import { getErrorMessage } from '@/service/api/_getErrorMessage'
+
+export default function SignInGoogleButton() {
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    if (url) {
+      window.location.href = url
+    }
+  }, [url])
+
+  const handleGetGoogleLoginUrl = () => {
+    AuthService.getGoogleLoginUrl()
+      .then((response) => {
+        const redirectUrl: string = response?.data
+        setUrl(redirectUrl)
+      })
+      .catch((error) => {
+        const { message } = getErrorMessage(error)
+        messageError(message)
+      })
+  }
+
+  return (
+    <Button
+      block
+      size='lg'
+      onClick={handleGetGoogleLoginUrl}
+      title='Continue with google'
+      leadingIcon={<Image src={GoogleIcon} alt='google-image' className='mr-4 size-6' />}
+    />
+  )
+}
